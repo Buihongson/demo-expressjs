37fbfb6f177bc7f4706abb94144edebb9472fe21
sonhong <sonbui151097@gmail.com>
2018-10-16 22:59:03 +0700

    Add lowdb and repair mothod get and post

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express');
-var app = express();
-var port = 3000;
+var app = express(); 
+var port = 3000;   
+var bodyParser = require('body-parser');
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+var adapter = new FileSync('db.json');
+
+db = low(adapter);
+
+// Set some defaults
+db.defaults({ users: [] })
+  .write();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-var users = [
-    { id : 1 , name : 'son'},
-    { id : 2 , name : 'huyen'}
-]
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function(req , res){
     res.render('index', {
@@ -18,28 +26,31 @@ app.get('/', function(req , res){
 
 app.get('/users', function(req , res){
     res.render('users/index', {
-      users : users
+      users : db.get('users').value()
     });
 });
 
 app.get('/users/search' , function(req , res){
     var q = req.query.q;
-    var matchedUsers = users.filter(function(user){
+    var matchedUsers = db.get('users').filter(function(user){
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
+    }).value();
     
+    console.log(matchedUsers);
+
     res.render('users/index', {
         users: matchedUsers,
-        keyWord: q 
+        keyWord: q  
     });
 });
 
 app.get('/users/create' , function(req , res){
-    res.render('users/create')
+    res.render('users/create');
 });
 
-app.post('/user/create' , function(req , res){
-
+app.post('/users/create' , function(req , res){
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
 });
 
 app.listen(3000, function(){
