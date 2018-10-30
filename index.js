9a3ed0225872e2ec9bf104cc2df0072c126799cf
sonhong <sonbui151097@gmail.com>
2018-10-30 22:53:48 +0700

    Create page product and dont pagination for page

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 
 var userRoutes = require('./routes/user.route');
 var authRoutes = require('./routes/auth.route');
+var productRoutes = require('./routes/product.route');
 
 var authMiddleware = require('./middleware/auth.middleware');
 
@@ -29,6 +30,7 @@ app.get('/', function(req , res){
 
 app.use('/users' , authMiddleware.requireAuth , userRoutes);
 app.use('/auth' , authRoutes);
+app.use('/products' , productRoutes);
 
 app.listen(3000, function(){
    console.log('Server listening on port ' + port); 
