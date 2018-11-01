f728ffe7f96376bf00b747d165ef260a104b59f6
sonhong <sonbui151097@gmail.com>
2018-11-01 21:47:49 +0700

    Update pagination for page products

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -3,11 +3,30 @@ var db = require('../db');
 module.exports.index = function (req , res){
     var page = parseInt(req.query.page) || 1; //n
     var perPage = 8; //x
+    var arr = [];
     
     var start = (page - 1) * perPage;
     var end = page * perPage;
 
+    var totalProduct = Math.ceil(db.get('products').value().length/perPage);
+
+    if(page === 1){
+        arr.push(page,page+1,page+2,'Next','Last')
+    }
+
+    if(page > 1 && page < totalProduct){
+        arr.push('First' , 'Prev' , page - 1 , page , page + 1 , 'Next' , 'Last')
+    }
+
+    if(page === totalProduct){
+        arr.push('First', 'Prev' , page - 2 , page - 1 , page)
+    }
+
     res.render('products' , {
-        products: db.get('products').value().slice(start , end)
+        products: db.get('products').value().slice(start , end),
+        page: page,
+        totalPages: arr,
+        lastPage: totalProduct
     });
 };
+
